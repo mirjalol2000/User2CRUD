22ee115cc4d9f33ee5ae56b1663fe834db905a8a
hicadok105@ridteam.com <hicadok105@ridteam.com>
2023-12-11 21:39:34 +0500

    Create Services User Add

---
+++ Services/Foundations/Users/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using User2CRUD.Models.Users;
+
+namespace User2CRUD.Services.Foundations.Users
+{
+    public interface IUserService
+    {
+        User AddUser(User user);    
+    }
+}
---
+++ Services/Foundations/Users/UserService.cs
@@ -0,0 +1,19 @@
+﻿using User2CRUD.Brokers.Storages;
+using User2CRUD.Models.Users;
+
+namespace User2CRUD.Services.Foundations.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public UserService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public User AddUser(User user)=>
+            this.storageBroker.InsertUser(user);
+        
+    }
+}
