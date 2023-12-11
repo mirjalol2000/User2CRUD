47ec8208f8bd8bb93adc0ad0a413306583084f84
hicadok105@ridteam.com <hicadok105@ridteam.com>
2023-12-11 20:54:21 +0500

    INFRA:Add Model User

---
+++ Models/Users/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace User2CRUD.Models.Users
+{
+    public class Users
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
