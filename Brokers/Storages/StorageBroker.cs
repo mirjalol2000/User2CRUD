9d42994745bd90e12f599c8aa2b76c162834b9b6
hicadok105@ridteam.com <hicadok105@ridteam.com>
2023-12-11 21:23:11 +0500

    Create Brokers Storages

---
+++ Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,30 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using User2CRUD.Models.Users;
+
+namespace User2CRUD.Brokers.Storages
+{
+    public class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        public DbSet<User> Users { get; set; }
+
+        public StorageBroker()=>
+            this.Database.EnsureCreated();        
+        
+
+
+        public User InsertUser(User user)
+        {
+            this.Users.Add(user);
+            this.SaveChanges(); 
+
+            return user;    
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source : User2CRUD.db";
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
